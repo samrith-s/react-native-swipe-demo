fbdceeb34c8484443d47dcec7540458550486679
Samrith Shankar <samrith.s@media.net>
2019-02-03 05:16:06 +0530

    Restructured app

--- App.js
+++ App.js
@@ -1,21 +1,2 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello Trishla!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import App from './src';
+export default App;
