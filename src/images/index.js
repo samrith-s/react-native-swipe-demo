bf48fd8ab5b390cc13ac897af7693b672a353742
Samrith Shankar <samrith.s@media.net>
2019-02-03 05:41:01 +0530

    Moved images to a JS file

---
+++ src/images/index.js
@@ -0,0 +1,15 @@
+import apple from './apple.png';
+import blueberries from './blueberries.png';
+import coconut from './coconut.png';
+import grapes from './grapes.png';
+import lime from './lime.png';
+import orange from './orange.png';
+
+export default {
+    apple,
+    blueberries,
+    coconut,
+    grapes,
+    lime,
+    orange
+};
